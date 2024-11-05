ed06735601fc9933e5c692e7df536d62dba573ea
AA2026 <aayanali2608@gmail.com>
2024-11-04 20:49:06 -0600

    Old changes...ignore

--- my-expo-project/firebaseConfig.js
+++ my-expo-project/firebaseConfig.js
@@ -1,16 +1,16 @@
 import firebase from "firebase/app";
 import "firebase/auth";
-import { config } from "dotenv";
+import Constants from 'expo-constants';
 
-config();
+const { firebaseApiKey, firebaseAuthDomain, firebaseProjectId, firebaseStorageBucket, firebaseMessagingSenderId, firebaseAppId } = Constants.manifest.extra;
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: firebaseApiKey,
+  authDomain: firebaseAuthDomain,
+  projectId: firebaseProjectId,
+  storageBucket: firebaseStorageBucket,
+  messagingSenderId: firebaseMessagingSenderId,
+  appId: firebaseAppId,
 };
 
 if (!firebase.apps.length) {
@@ -18,3 +18,4 @@ if (!firebase.apps.length) {
 }
 
 export { firebase };
+
