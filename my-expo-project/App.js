2ba88f5ac9bc0d6941171be84987c4d6455291f3
AA2026 <aayanali2608@gmail.com>
2024-11-04 20:49:51 -0600

    Ignore...old changes

--- my-expo-project/App.js
+++ my-expo-project/App.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { firebase } from '/firebaseConfig.js';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import AuthScreen from './Components/AuthScreen'; // Import your AuthScreen
-import Homepage from './Components/Homepage';
+import AuthScreen from './Components/AuthScreen'; 
 import OpportunitiesPage from './Components/OpportunitiesPage';
 
 const Stack = createNativeStackNavigator();
@@ -11,8 +10,8 @@ const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Auth"> {/* Change to Auth */}
-        <Stack.Screen name="Auth" component={AuthScreen} /> {/* Add AuthScreen */}
+      <Stack.Navigator initialRouteName="Auth">
+        <Stack.Screen name="Auth" component={AuthScreen} />
         <Stack.Screen name="Home" component={Homepage} />
         <Stack.Screen name="Opportunities" component={OpportunitiesPage} />
       </Stack.Navigator>
