83a6cafe3a867796cb772230f3c127a0a7177ec4
WOZohaib <winneronezohaib@gmail.com>
2025-02-27 22:44:27 +0000

    Email Logic securely added

--- my-expo-project/functions/index.js
+++ my-expo-project/functions/index.js
@@ -5,8 +5,9 @@ const sgMail = require('@sendgrid/mail');
 // Initialize Firebase Admin SDK
 admin.initializeApp();
 
-// Set SendGrid API key
-sgMail.setApiKey('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'); // Replace with your SendGrid API Key
+// Retrieve SendGrid API key from Firebase environment variables
+const sendGridKey = functions.config().sendgrid.key;
+sgMail.setApiKey(sendGridKey);
 
 // Firebase function to send email
 exports.sendEmail = functions.https.onCall(async (data, context) => {
@@ -14,17 +15,16 @@ exports.sendEmail = functions.https.onCall(async (data, context) => {
 
   const msg = {
     to: to,
-    from: 'winneronezohaib@gmail.com', // Use your verified SendGrid email address
+    from: 'winneronezohaib@gmail.com', // Use your verified SendGrid email
     subject: subject,
     text: message,
   };
 
   try {
-    // Send email using SendGrid
     await sgMail.send(msg);
     return { success: true, message: 'Email sent successfully!' };
   } catch (error) {
-    console.error('Error sending email: ', error);
+    console.error('Error sending email:', error);
     return { success: false, message: 'Error sending email.' };
   }
 });
